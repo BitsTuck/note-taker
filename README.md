30dfbb13b4321a107a8747a86e5775bf6353391e
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-27 09:08:52 -0400

    final server.js with comments, and readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # note-taker
+
+Version 1
+Deployed 27 October 2023
+
+
+<img width="1471" alt="Screenshot 2023-10-27 at 08 53 10" src="https://github.com/BitsTuck/note-taker/assets/144712161/12761bec-7ced-430f-9f54-d5893561f72d">
+
+## Description
+
+The Challenge for Module #11 asked us to modify back-end code to make provided front-end code functional. In order to be successful, the index page needed to link to a notes page, that would accept and store note data.
+
+## Usage
+
+To use this app, click on the deployment link. From the landing page, click on Get Started to be taken to the notes page. Type in your note title, and any additional notes you might have. The Save button will appear when there is content in both fields. Save the note, and refresh the page to see your note saved on the lefthand pane. If you click on the notes in the lefthand pane, they will appear (with all of your detail) on the right.
+
+
+## Challenges
+
+It is taking some time for my brain to understand that we are launching the index page and making the links function is something that we now do through express, not the methods we were using earlier in class. The file structure had me confused for a bit, but once my tutor pointed out my errors, everything started functioning smoothly. I tried to figure out the delete bonus, but couldn't quite make it work the way I wanted in time for submission.
+
+
+
+## Credits
+
+Tutor Dru Sanchez provided guidance.
+
+
+
+## License
+
+Please refer to the license in the repo.
+
+---
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//All necessary Node packages and routes
 const express = require ('express');
 const fs = require ('fs');
 const {v4: uuidv4} = require ('uuid');
@@ -12,20 +13,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
+//Connects index to notes page
 app.get('/notes', (req, res) => {
-    console.log('line 15 hit')
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+//retrieves existing note data
 app.get('/api/notes', (req, res) => res.json (noteData));
 
+//Takes you to the index page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html' ));
 });
 
 
-
+//adds a new note by taking in the user input, calling the existing notes, adding the user input to the existing notes, and then re-writing the whole file to include both
 app.post('/api/notes', (req, res) => {
     console.log(req.body)
     const {title, text} = req.body;
@@ -48,10 +50,28 @@ app.post('/api/notes', (req, res) => {
     })
 });
 
-app.delete('/api/notes', (req, res) => {
+// app.delete('/api/notes:id', (req, res) => {
+//     const {title, text} = req.body;
+//     const deleteNote = {
+//         title : '',
+//         text: '',
+//         id: '',
+//     }
+//     fs.readFile('./db/db.json', 'utf8', (err, oldNotes) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             const parsedNotes = JSON.parse(oldNotes);
+//             parsedNotes.slice(req.body);
+//             fs.writeFile('./db/db.json', JSON.stringify(parsedNotes),
+//             (err) =>
+//             err ? console.err(err) : res.json(parsedNotes))
+            
+//         }
+//     })
 
-})
+// })
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+    console.log(`Server running on PORT ${PORT}`)
 })
